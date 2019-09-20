ade8f4aacd4167ca41a6fff9ddbe4ca767dfd7a3
David Alejandro Gómez Cifuentes <david.gomez@ceiba.com.co>
2019-09-20 10:15:47 -0500

    Solicionando problemas de librerias para CI

--- ParqueaderoElDesfalco.Droid/obj/Debug/android/src/mono/android/app/environment.armeabi-v7a.s
+++ ParqueaderoElDesfalco.Droid/obj/Debug/android/src/mono/android/app/environment.armeabi-v7a.s
@@ -85,7 +85,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"1569ba47-8c44-44cd-9b44-ad6386412a72"
+	.asciz	"407fe025-3844-47df-9ba2-297229cfd3a5"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
