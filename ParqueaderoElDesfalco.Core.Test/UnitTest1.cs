cfd4e0e1844b0c68809d3633de9da7a6ff3c97b3
David Alejandro Gómez Cifuentes <david.gomez@ceiba.com.co>
2019-09-20 08:16:34 -0500

    Se adiciona una prueba de ensayo para evidenciar el Pipeline de CI

--- ParqueaderoElDesfalco.Core.Test/UnitTest1.cs
+++ ParqueaderoElDesfalco.Core.Test/UnitTest1.cs
@@ -6,9 +6,9 @@ namespace ParqueaderoElDesfalco.Core.Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Test1ForExample()
         {
-            
+            Assert.Equal(1, 1);
         }
     }
 }
